8f196d4116c1a9b6a6842ec1b371509156d79e77
Rajmogli <142821767+Rajmogli@users.noreply.github.com>
2023-08-30 11:43:20 +0530

    Update TDS_Assignement8.py

--- TDS_Assignement8.py
+++ TDS_Assignement8.py
@@ -8,5 +8,6 @@ num1 = st.number_input("Enter the first number:")
 num2 = st.number_input("Enter the second number:")
 num3 = st.number_input("Enter the third number:")
 
-largest = find_largest(num1, num2, num3)
+#largest = find_largest(num1, num2, num3)
+largest = max(num1, num2, num3)
 st.write("The largest number is: {largest}")
