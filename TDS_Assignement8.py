d347451aa1271576b2c0901e14f2b4cb1660c749
Rajmogli <142821767+Rajmogli@users.noreply.github.com>
2023-08-30 11:35:05 +0530

    Update TDS_Assignement8.py

--- TDS_Assignement8.py
+++ TDS_Assignement8.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 st.text("Hello Raj Mogli")
 
+st.title("Find the Largest Number")
+num1 = st.number_input("Enter the first number:")
+num2 = st.number_input("Enter the second number:")
+num3 = st.number_input("Enter the third number:")
